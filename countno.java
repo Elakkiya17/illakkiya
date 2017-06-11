10bb361501f911709ac56aaf59e7b773915d3b8a
Elakkiya17 <illakkiya97@gmail.com>
2017-06-11 19:20:01 +0530

    Create countno.java

---
+++ countno.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+class countno {
+	public static void main (String[] args) {
+	   int n;
+	   Scanner sc=new Scanner(System.in);
+	   n=sc.nextInt();
+	   String s=String.valueOf(n);
+	   int l=s.length();
+	   System.out.println(l);
+
+	}
+}
