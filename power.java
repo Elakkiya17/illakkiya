2b52f64ece71d9d1a86d8063c25e4a2abe33d951
Elakkiya17 <illakkiya97@gmail.com>
2017-06-12 21:51:18 +0530

    Create power.java

---
+++ power.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+class powerofno {
+	public static void main (String[] args) {
+	    int p,n;
+	    double ans;
+	    Scanner sc=new Scanner(System.in);
+	    p=sc.nextInt();
+	    n=sc.nextInt();
+	    //ans=Math.pow(n,p);
+	    System.out.println(Math.pow(n,p));
