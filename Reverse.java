b7bde355f94c27edfa53a01728ee3a77326cca4f
Elakkiya17 <illakkiya97@gmail.com>
2017-08-18 15:04:22 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Reverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String s=Integer.toString(n);
+		StringBuffer sb=new StringBuffer(s);
+		System.out.println(sb.reverse());
+		
+
+	}
+
+}
