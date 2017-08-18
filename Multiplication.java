9f071d9456668e98b01cc5553e941ecd62eee963
Elakkiya17 <illakkiya97@gmail.com>
2017-08-18 14:23:15 +0530

    Create Multiplication.java

---
+++ Multiplication.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int m=sc.nextInt();
+		int p;
+		for(int i=1;i<=m;i++)
+		{
+			p=i*n;
+			System.out.println(p);
+		}
+		
+
+	}
+
+}
