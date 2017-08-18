2648765717a39ab3645f5a2c9404e9364ab75cbf
Elakkiya17 <illakkiya97@gmail.com>
2017-08-18 22:26:11 +0530

    Create Numberofchar.java

---
+++ Numberofchar.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Numberofchar {
+
+	public static void main(String[] args) 
+	{
+      Scanner sc=new Scanner(System.in);
+      String s=sc.next();
+      int l=s.length();
+      System.out.println(l);
+	}
+
+}
